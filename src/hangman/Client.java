57aa276837ed5048aabcdc773cd80497e59f1a03
marion <ericsamama@n172-p75.kthopen.kth.se>
2013-11-08 19:06:00 +0100

    Debut du main, Client, Server

---
+++ src/hangman/Client.java
@@ -0,0 +1,36 @@
+package hangman;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	
+	private String address;
+	private int port;
+
+	public Client(String address, int port) {
+		// TODO Auto-generated constructor stub
+		this.address = address;
+		this.port = port;
+	
+	try {
+		Socket socket = new Socket(address,port);
+		socket.setSoTimeout(10000);
+		PrintWriter wr = new PrintWriter(socket.getOutputStream());
+		//wr.println("BITE !!");
+		//wr.flush();
+		socket.close();
+		
+	} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}
+
+}
--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -3,8 +3,12 @@ package hangman;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("OK");
+		String address = "127.0.0.1"; 
+		int port = 10000;
+		
+		Server server = new Server();
+		
+		Client client = new Client(address, port);
 	}
 
 }
