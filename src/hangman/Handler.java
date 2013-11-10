529f045a8d1a160afae5e1138fddc56c255a1801
phacks <mrtownshend@gmail.com>
2013-11-10 01:37:27 +0100

    Sélection d'un mot aléatoire à la connexion d'un client

--- src/hangman/Handler.java
+++ src/hangman/Handler.java
@@ -1,7 +1,11 @@
 package hangman;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Handler extends Thread{
 
@@ -12,7 +16,52 @@ public class Handler extends Thread{
 	}
 	
 	public void run(){
-		System.out.println("Foutre.");
+		
+		try {
+			int nbWords = countWords("words");
+			String word = selectWord("words", nbWords);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private int countWords(String fileName) throws IOException{
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(new FileInputStream(fileName));
+		 int c = 0;
+		    try {
+		      while (scanner.hasNextLine()){
+		    	scanner.nextLine();
+		    	c++;
+		      }
+		    }
+		    finally{
+		      scanner.close();
+		    }
+			return c;
+		
+	}
+
+	private String selectWord(String fileName, int nbWords) throws IOException {
+		    Scanner scanner = new Scanner(new FileInputStream(fileName));
+		    int c = 0;
+		    int r = (int) Math.round(Math.random() * nbWords);
+		    String word = "";
+		    try {
+		      while (scanner.hasNextLine()){
+		    	c++;
+		    	String nextLine = scanner.nextLine();
+		    	if (c == r){
+		    		word = nextLine;
+		    	}
+		      }
+		    }
+		    finally{
+		      scanner.close();
+		    }
+		    return word;
 	}
 
 }
--- src/hangman/ServerHangman.java
+++ src/hangman/ServerHangman.java
@@ -32,6 +32,7 @@ public class ServerHangman {
 			}
 			catch (SocketException e) { e.printStackTrace(); }
 		}
+		
 
 	}
 
